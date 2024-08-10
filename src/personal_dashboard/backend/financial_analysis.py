19d0646f5c3760e14c945cb3033603fe6eb92358
Thomas <tomatheickal@hotmail.com>
2024-08-10 16:19:59 +0100

    Last month completed

--- src/personal_dashboard/backend/financial_analysis.py
+++ src/personal_dashboard/backend/financial_analysis.py
@@ -1,25 +1,79 @@
+from typing import Hashable, Set
+
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
 
 
-def financial_analysis(df, exclude_holiday=False):
-    key_figures = {}
-    df["transaction_time"] = pd.to_datetime(df["transaction_time"])
-    df.set_index("transaction_time", inplace=True)
+class FinancialAnalysis:
 
-    if exclude_holiday:
-        df = df[df["category"] != "Holiday"]
-    # Identify the top expense categories
-    top_expenses = (
-        df.groupby("category")["amount_gbp"].sum().sort_values(ascending=False)
-    )
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
 
-    # Calculate average monthly income and expenses
-    monthly_expenses = df.resample("ME")["amount_gbp"].sum().mean()
+        self.df["transaction_time"] = pd.to_datetime(self.df["transaction_time"])
+        self.df.set_index("transaction_time", inplace=True)
 
-    key_figures["Top Expense Categories"] = {
-        category: f"£{amount:,.2f}" for category, amount in top_expenses.head().items()
-    }
-    key_figures["Average Monthly Expenses"] = f"£{monthly_expenses:,.2f}"
-    return key_figures
+    def __get_last_day_of_last_month(self) -> pd.Timestamp:
+        current_date = pd.Timestamp("now")
+        first_day_of_current_month = current_date.replace(day=1)
+        last_day_of_last_month = first_day_of_current_month - pd.Timedelta(days=1)
+        return last_day_of_last_month
+
+    def get_last_month_df(self) -> pd.DataFrame:
+        last_day_of_last_month = self.__get_last_day_of_last_month()
+        first_day_of_last_month = last_day_of_last_month.replace(day=1)
+        df_last_month = self.df[
+            (self.df.index >= first_day_of_last_month)
+            & (self.df.index <= last_day_of_last_month)
+        ]
+
+        return df_last_month
+
+    def get_two_months_before_df(self) -> pd.DataFrame:
+        last_day_of_last_month = self.__get_last_day_of_last_month()
+        first_day_of_last_month = last_day_of_last_month.replace(day=1)
+        last_day_of_month_before_last = first_day_of_last_month - pd.Timedelta(days=1)
+        first_day_of_month_before_last = last_day_of_month_before_last.replace(day=1)
+
+        df_month_before_last = self.df[
+            (self.df.index >= first_day_of_month_before_last)
+            & (self.df.index <= last_day_of_month_before_last)
+        ]
+
+        return df_month_before_last
+
+    def get_total_expense(self, df: pd.DataFrame):
+        last_month_expense = df["amount_gbp"].sum()
+        return last_month_expense
+
+    def get_top_expense_and_description(self, df: pd.DataFrame):
+        top_expense_row = df.loc[df["amount_gbp"].idxmax()]
+        top_expense_amount = top_expense_row["amount_gbp"]
+        top_expense_description = top_expense_row["description"]
+        return top_expense_amount, top_expense_description
+
+    def get_top_expense_categories(self, df: pd.DataFrame) -> dict[Hashable, str]:
+        category_spending = df.groupby("category")["amount_gbp"].sum()
+        top_categories = category_spending.sort_values(ascending=False)
+        top_categories_expense_amount = {
+            category: f"£{amount:,.2f}"
+            for category, amount in top_categories.head().items()
+        }
+        return top_categories_expense_amount
+
+    def get_average_expense(self, period: str):
+        average_expenses = self.df.resample(period)["amount_gbp"].sum().mean()
+        return average_expenses
+
+    def get_percentage_diff_between_last_two_months(self):
+        last_month_df = self.get_last_month_df()
+        two_months_ago_df = self.get_two_months_before_df()
+
+        last_month_expense = self.get_total_expense(last_month_df)
+        two_months_ago_expense = self.get_total_expense(two_months_ago_df)
+
+        percentage_difference = (
+            (last_month_expense - two_months_ago_expense) / two_months_ago_expense
+        ) * 100
+
+        return percentage_difference
--- src/personal_dashboard/frontend/dashboard.py
+++ src/personal_dashboard/frontend/dashboard.py
@@ -1,73 +1,143 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
 
 from src.personal_dashboard.backend.database import SqlConnections
-from src.personal_dashboard.backend.financial_analysis import financial_analysis
+from src.personal_dashboard.backend.financial_analysis import FinancialAnalysis
 
 
-def plot_category_wise_spending_analysis(df: pd.DataFrame, exclude_holiday=False):
-    if exclude_holiday:
-        df = df[df["category"] != "Holiday"]
+class Figures:
+    def __init__(
+        self,
+        df: pd.DataFrame,
+        finance_analyzer: FinancialAnalysis,
+        exclude_holiday=True,
+    ):
+        self.df = df
+        self.exclude_holiday = exclude_holiday
+        if self.exclude_holiday:
+            self.df = self.df[self.df["category"] != "Holiday"]
+        self.finance_analyzer = finance_analyzer
 
-    st.markdown("4. Category-wise Spending Analysis")
-    expenses_by_category = df.groupby("category")["amount_gbp"].sum()
+    def category_spending_pie_chart(self):
 
-    expenses_by_category.plot(
-        kind="pie", figsize=(10, 8), autopct="%1.1f%%", startangle=140
-    )
-    plt.title("Expenses Analysis")
-    plt.ylabel("")  # Hide the y-label as it's unnecessary for pie charts
-    # plt.savefig("data/expense_category_analysis.png", bbox_inches="tight")
-    st.pyplot(plt)
+        category_expenses = (
+            self.df.groupby("category")["amount_gbp"].sum().abs().reset_index()
+        )
+
+        fig = px.pie(
+            category_expenses,
+            values="amount_gbp",
+            names="category",
+            color_discrete_sequence=px.colors.sequential.RdBu,
+            width=700,
+            height=300,
+        )
+        fig.update_layout(
+            margin=dict(l=20, r=20, t=20, b=20),
+        )
+        st.plotly_chart(fig)
+
+    def top_category_spending_table(self):
+        # Display top expense categories in a table
+
+        df_reset = self.df.reset_index(drop=True)
+        expenses_df = pd.DataFrame(
+            list(self.finance_analyzer.get_top_expense_categories(df_reset).items()),
+            columns=["Category", "Amount"],
+        )
+        st.markdown("")
+        st.markdown("")
+        st.table(expenses_df)
+
+
+class PageComponents:
+
+    def __init__(self, df: pd.DataFrame, exclude_holiday=False) -> None:
+        self.df = df
+        self.exclude_holiday = exclude_holiday
+        if self.exclude_holiday:
+            self.df = self.df[self.df["category"] != "Holiday"]
+
+        self.finance_analyzer = FinancialAnalysis(self.df)
+
+    def title(self):
+        st.title("My Local AI Finance Insighter")
+        st.markdown(
+            "**A personalized and secure approach to analyzing financial data, providing insights and recommendations tailored to individual needs.**"
+        )
+
+    def last_week(self):
+        pass
+
+    def last_month(self):
+        last_month_df = self.finance_analyzer.get_last_month_df()
+        col1, col2, col3 = st.columns(3)
+
+        average_monthly_expense = self.finance_analyzer.get_average_expense("ME")
+        last_month_expense = self.finance_analyzer.get_total_expense(last_month_df)
+        last_month_top_expense_amount, last_month_top_expense_description = (
+            self.finance_analyzer.get_top_expense_and_description(last_month_df)
+        )
+        percentage_diff_last_two_months = (
+            self.finance_analyzer.get_percentage_diff_between_last_two_months()
+        )
+        col1.metric(
+            label="Average Monthly Expense",
+            value=f"£{average_monthly_expense:,.2f}",
+        )
+        col2.metric(
+            label="Last Month Total Expense",
+            value=f"£{last_month_expense:,.2f}",
+            delta=f"{percentage_diff_last_two_months:,.2f}% from month before",
+            delta_color="inverse",
+        )
+        col3.metric(
+            label="Last Month Top Expense",
+            value=f"£{last_month_top_expense_amount:,.2f}",
+            delta=last_month_top_expense_description,
+            delta_color="off",
+        )
+        st.divider()
+        # # Display top expense categories in a table
+        col1, col2 = st.columns(2)
+        last_month_figures = Figures(last_month_df, self.finance_analyzer)
+
+        with col1:
+            st.markdown(
+                "<h4 style='text-align: left; color: white;'>Top Expenses by Category</h4>",
+                unsafe_allow_html=True,
+            )
+            last_month_figures.top_category_spending_table()
+        with col2:
+            st.markdown(
+                "<h4 style='text-align: left; color: white;'>Percentage of Expenses by Category</h4>",
+                unsafe_allow_html=True,
+            )
+            last_month_figures.category_spending_pie_chart()
+
+    def budget_analysis(self):
+        pass
+
+    def stats(self):
+        pass
 
 
 def streamlit_app(df: pd.DataFrame, exclude_holiday=False):
-    st.title("My Local AI Finance Insighter")
-    st.markdown(
-        "**A personalized and secure approach to analyzing financial data, providing insights and recommendations tailored to individual needs.**"
-    )
-
-    analysis_results = financial_analysis(df, exclude_holiday)
-    results_str = ""
-    # Loop through the dictionary
-    for key, value in analysis_results.items():
-        if isinstance(value, dict):
-            # If the value is another dictionary, further iterate to get sub-keys and values
-            sub_results = ", ".join(
-                [f"{sub_key}: {sub_value}" for sub_key, sub_value in value.items()]
-            )
-            results_str += f"{key}: {sub_results}\n"
-        else:
-            # For direct key-value pairs, simply concatenate
-            results_str += f"{key}: {value}\n"
-
-    # Display average monthly figures
-    st.subheader("Average Monthly Figures")
-    col1, col2 = st.columns(2)
-    col1.metric(
-        label="Average Monthly Expenses",
-        value=analysis_results["Average Monthly Expenses"],
-    )
-    col2.metric(
-        label="Average Monthly Expenses",
-        value=analysis_results["Average Monthly Expenses"],
-    )
-
-    # Display top expense categories in a table
-    st.subheader("Top Expense Categories")
-    expenses_df = pd.DataFrame(
-        list(analysis_results["Top Expense Categories"].items()),
-        columns=["Category", "Amount"],
-    )
-    st.table(expenses_df)
-
-    with st.container():
-        col1, col2 = st.columns(2)
-        with col1:
-            plot_category_wise_spending_analysis(df, exclude_holiday)
-        with col2:
-            plot_category_wise_spending_analysis(df, exclude_holiday)
+    st.set_page_config(layout="wide")
+    components = PageComponents(df, exclude_holiday)
+    components.title()
+    tab1, tab2, tab3, tab4 = st.tabs(["Last Week", "Last Month", "Stats", "Budget"])
+    with tab1:
+        components.last_month()
+    # with st.container():
+    #     col1, col2 = st.columns(2)
+    #     with col1:
+    #         plot_category_wise_spending_analysis(df, exclude_holiday)
+    #     with col2:
+    #         plot_category_wise_spending_analysis(df, exclude_holiday)
 
 
 if __name__ == "__main__":
